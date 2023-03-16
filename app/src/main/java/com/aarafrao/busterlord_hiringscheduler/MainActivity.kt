fe0c3bbf9abece6059f0eccfc2f2660e2a99252d
aaraf.younus <aarafrao22@gmail.com>
2023-03-16 14:28:12 +0500

    just show today click event then make sure everything is fine.

--- app/src/main/java/com/aarafrao/busterlord_hiringscheduler/MainActivity.kt
+++ app/src/main/java/com/aarafrao/busterlord_hiringscheduler/MainActivity.kt
@@ -65,7 +65,7 @@ class MainActivity : AppCompatActivity(), ClickListener {
 
 
                     if (currentAppointment.date == formattedDate) {
-                        //
+                        //here
 
 
                     }
